ff324a6127ac220987603570d3581e733cee4798
oath aphisit <aphisit.thu@turbo.co.th>
2024-06-18 01:35:17 +0700

    add : default user

--- client/src/constants/auth.ts
+++ client/src/constants/auth.ts
@@ -7,17 +7,57 @@ export const defaultUsers: DefaultUserUtil[] = [
         admin: true,
         request:
             {
-                username: 'defualt@admin',
+                username: 'default@admin',
                 password: '123456789'
             }
     },
     {
-        title: 'User',
+        title: 'User 1',
         description: 'default user account',
         admin: false,
         request:
             {
-                username: 'defualt@user',
+                username: 'default@user1',
+                password: '123456789'
+            }
+    },    
+    {
+        title: 'User 2',
+        description: 'default user account',
+        admin: false,
+        request:
+            {
+                username: 'default@user2',
+                password: '123456789'
+            }
+    },  
+    {
+        title: 'User 3',
+        description: 'default user account',
+        admin: false,
+        request:
+            {
+                username: 'default@user3',
+                password: '123456789'
+            }
+    },    
+    {
+        title: 'User 4',
+        description: 'default user account',
+        admin: false,
+        request:
+            {
+                username: 'default@user4',
+                password: '123456789'
+            }
+    },   
+    {
+        title: 'User 5',
+        description: 'default user account',
+        admin: false,
+        request:
+            {
+                username: 'default@user5',
                 password: '123456789'
             }
     },
